782fe37729a71dbba2fbe943b83f5b9e9734cfdb
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2024-05-05 17:11:26 -0500

    Se agrega la linea de impresión con el nombre completo

--- Practica.java
+++ Practica.java
@@ -2,5 +2,6 @@ public class Practica {
     public static void main (String[] args){
         System.out.println("Hola Mundo!");
         System.out.println("Bienvenidos al curso");
+        System.out.println("Max Junior Soncco Mamani");
     }
 }
