865bd33ba14bd56ee8e5dadebd48a98d5faf0bcf
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2024-05-12 11:46:55 -0500

    Se mueve la clase Scanner fuera de los métodos

--- lab02/Ejercicio02.java
+++ lab02/Ejercicio02.java
@@ -1,12 +1,12 @@
 import java.util.*;
 public class Ejercicio02{
+    Scanner sc = new Scanner(System.in);
     private ArrayList <Integer> rotacion = new ArrayList<>();
     public static void main (String[] args){
 
     }
 
     public void ingresarElementos (){
-        Scanner sc = new Scanner(System.in);
         System.out.println("Inidicar cantidad de elementos del arreglo");
         int cantidadElementos = sc.nextInt();
 
