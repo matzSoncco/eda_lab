28d90f46d33b48c86d4ce8b4d17fe2d2dea9e869
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2024-05-12 11:08:50 -0500

    Se crea y sube la carpeta lab02 para el laboratorio y se sube el Ejercicio01 tambien

---
+++ lab02/Ejercicio01.java
@@ -0,0 +1,29 @@
+//INVERTIR UN ARREGLO DE ENTEROS
+import java.util.*;
+public class Ejercicio01 {
+    ArrayList<Integer> invertir = new ArrayList<>();
+    public static void main (String[] args){
+        Ejercicio01 ejercicio = new Ejercicio01();
+        ejercicio.ingresar(); // Se llama al método 'ingresar'
+        ejercicio.invertir(); // Se llama al método 'invertir'
+    }
+
+    public void ingresar (){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inidicar cantidad de elementos del arreglo");
+        int cantidadElementos = sc.nextInt();
+
+        System.out.println("Ingresar los elementos del arreglo");
+        for (int i = 0; i<cantidadElementos; i++){
+            int elemento = sc.nextInt();
+            invertir.add(elemento);
+        }
+    }
+
+    public void invertir (){
+        //Imprimir lista invertida
+        for (int i = invertir.size()-1; i >= 0; i--) {
+            System.out.println(invertir.get(i));
+        }
+    }
+}
